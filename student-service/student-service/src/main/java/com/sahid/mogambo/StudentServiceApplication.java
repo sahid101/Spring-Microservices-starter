6b7345c3b2d955766f461751e5841777628f8294
Sahid <sahidansari112344@gmail.com>
2025-08-09 03:22:33 +0530

    Registry -> Eureka Server

--- student-service/student-service/src/main/java/com/sahid/mogambo/StudentServiceApplication.java
+++ student-service/student-service/src/main/java/com/sahid/mogambo/StudentServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,19 +12,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
-@ComponentScan({
-		"com.sahid.mogambo.controller",
-		"com.sahid.mogambo.service"
-})
-@EntityScan({
-		"com.sahid.mogambo.entity"
-})
-@EnableJpaRepositories({
-		"com.sahid.mogambo.jpa"
-})
-@EnableFeignClients({
-		"com.sahid.mogambo.feignclients"
-})
+@ComponentScan({"com.sahid.mogambo.controller",	"com.sahid.mogambo.service"})
+@EntityScan({"com.sahid.mogambo.entity"})
+@EnableJpaRepositories({"com.sahid.mogambo.jpa"})
+@EnableFeignClients({"com.sahid.mogambo.feignclients"})
 public class StudentServiceApplication {
 
 	@Value("${address.service.url}")
